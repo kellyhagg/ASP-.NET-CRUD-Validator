ee3afd3c1442a74464517eb1691bd94dc64ca2f5
Kelly Hagg <112842439+kellyhagg@users.noreply.github.com>
2024-03-06 12:45:09 -0800

    Add first UnitTest content

--- MVC/SeleniumTests.Tests/UnitTest1.cs
+++ MVC/SeleniumTests.Tests/UnitTest1.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 
-namespace SeleniumTests.Tests
+namespace seleniumTest.Tests
 {
     [TestClass]
     public class UnitTest1
@@ -9,6 +11,20 @@ namespace SeleniumTests.Tests
         [TestMethod]
         public void TestMethod1()
         {
+            //string url = "http://localhost:49198/Clients/Create";
+            string url = "https://localhost:44373";
+            //string url = "http://localhost:49198";
+            ChromeDriver driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl(url);
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+            driver.FindElement(By.LinkText("Client")).Click();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+            driver.FindElement(By.LinkText("Create New")).Click();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+            driver.FindElement(By.Id("Name")).SendKeys("Marge Simpson");
+            driver.FindElement(By.Id("Address")).SendKeys("Springfield");
+            driver.FindElement(By.XPath("//Input[@type='submit']")).Click();
         }
     }
 }
