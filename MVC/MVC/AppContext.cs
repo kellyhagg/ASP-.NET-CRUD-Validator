ec078a91c5556ad2946184cd2cb9fa022274269a
Kelly Hagg <112842439+kellyhagg@users.noreply.github.com>
2024-03-05 21:05:00 -0800

    Initial commit

---
+++ MVC/MVC/AppContext.cs
@@ -0,0 +1,18 @@
+﻿using MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MVC
+{
+    public class AppContext : DbContext
+    {
+        public AppContext()
+        : base("DefaultConnection")
+        {
+        }
+        public DbSet<Client> Clients { get; set; }
+    }
+}
